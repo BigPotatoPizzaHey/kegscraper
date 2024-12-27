4184604531436dd350242006111e397ac2fbcfc4
bpph <mfeise_c21@kegs.org.uk>
2024-12-27 23:18:11 +0000

    read license

--- setup.py
+++ setup.py
@@ -3,15 +3,18 @@ from setuptools import setup
 with open("README.md", 'r') as rmf:
     longdesc = rmf.read()
 
+with open("LICENSE", "r") as lf:
+    lisc = lf.read()
+
 setup(
     name='kegscraper',
     version='v0.1.3',
     packages=['kegscraper'],
     url='https://kegs.org.uk/',
-    license='unlicense',
+    license=lisc,
     author='BigPotatoPizzaHey',
     author_email="poo@gmail.com",
     description="The ultimate KEGS webscraping module",
     long_description=longdesc,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
 )
