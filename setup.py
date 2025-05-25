2018c56ff7f38ea56b42354e1f7d9dc4928f15dd
bpph <mfeise_c21@kegs.org.uk>
2025-05-25 22:09:39 +0100

    edit: move req.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup
 import os
 
-with open("README.md", 'r') as rmf:
-    longdesc = rmf.read()
-
-with open("LICENSE", "r") as lf:
-    lisc = lf.read()
-
 setup(
     name='kegscraper',
     version='v0.1.7',
-    packages=['kegscraper'] + [f"kegscraper.{subdir}" for subdir in next(os.walk("kegscraper"))[1] if subdir != "__pycache__"],
+    packages=['kegscraper'] +
+             [f"kegscraper.{subdir}" for subdir in next(os.walk("kegscraper"))[1] if subdir != "__pycache__"],
     url='https://kegs.org.uk/',
-    license=lisc,
+    license=open("LICENSE").read(),
     author='BigPotatoPizzaHey',
     author_email="poo@gmail.com",
     description="The ultimate KEGS webscraping module",
-    long_description=longdesc,
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    requires=[
+        'requests',
+        'bs4',
+        'beautifulsoup4',
+        'dateparser',
+        'setuptools',
+        'pypdf',
+        'pyperclip'
+    ]
 )
