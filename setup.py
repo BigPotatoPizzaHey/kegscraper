d6cb519fd3176a1386c4099c40084e1989ba1cf4
bpph <mfeise_c21@kegs.org.uk>
2025-05-25 22:20:02 +0100

    new ver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='kegscraper',
-    version='v0.1.9',
+    version='v0.1.10',
     packages=['kegscraper'] +
              [f"kegscraper.{subdir}" for subdir in next(os.walk("kegscraper"))[1] if subdir != "__pycache__"],
     url='https://kegs.org.uk/',
@@ -14,12 +14,12 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     requires=[
-        "requests~=2.32.3",
-        "bs4~=0.0.2",
-        "beautifulsoup4~=4.12.3",
-        "dateparser~=1.2.0",
-        "setuptools~=75.6.0",
-        "pypdf~=5.1.0",
-        "pyperclip~=1.9.0"
+        'requests',
+        'bs4',
+        'beautifulsoup4',
+        'dateparser',
+        'setuptools',
+        'pypdf',
+        'pyperclip'
     ]
 )
