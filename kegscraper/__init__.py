f781ba553a8242be7bae8161c56481479d1483dc
bpph <mfeise_c21@kegs.org.uk>
2025-06-09 18:09:58 +0100

    feat: kegscraper.activelearn - using playwright

--- kegscraper/__init__.py
+++ kegscraper/__init__.py
@@ -3,5 +3,5 @@ The ultimate KEGS webscraping experience.
 """
 from __future__ import annotations
 
-from . import bromcom, it, oliver, papercut, site, util, vle, kerboodle
+from . import bromcom, it, oliver, papercut, site, util, vle, kerboodle, activelearn
 from .util import exceptions
