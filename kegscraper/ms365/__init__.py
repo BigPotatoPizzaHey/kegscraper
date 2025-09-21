5622e66968f7127112003b13f7fecc7e8150ce50
BPPH <mfeise_c21@kegs.org.uk>
2025-09-21 14:34:20 +0100

    remove playwright install

--- kegscraper/ms365/__init__.py
+++ kegscraper/ms365/__init__.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import os
-import sys
-
 from .session import Session, login
 
-_result = os.system(f"\"{sys.executable}\" -m playwright install")
+# _result = os.system(f"\"{sys.executable}\" -m playwright install")
