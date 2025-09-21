e3bfb9e832508454936e3096c7a0f781607386dd
BPPH <mfeise_c21@kegs.org.uk>
2025-09-21 14:34:58 +0100

    remove playwright install

--- kegscraper/activelearn/__init__.py
+++ kegscraper/activelearn/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-import os
-import sys
 
 from .session import Session, login
 
-_result = os.system(f"\"{sys.executable}\" -m playwright install")
+# _result = os.system(f"\"{sys.executable}\" -m playwright install")
